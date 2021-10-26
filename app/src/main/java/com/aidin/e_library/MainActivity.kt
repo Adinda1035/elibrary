0304c3836b4558ea8514b96425aaf6f52550cf22
syabrinaaa <72056138+syabrinaaa@users.noreply.github.com>
2021-10-26 20:42:52 +0700

    update lagi

--- app/src/main/java/com/aidin/e_library/MainActivity.kt
+++ app/src/main/java/com/aidin/e_library/MainActivity.kt
@@ -3,7 +3,13 @@ package com.aidin.e_library
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.aidin.e_library.databinding.ActivityMainBinding
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -13,10 +19,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btLogin.setOnClickListener {
-            startActivity(Intent(this@MainActivity,Home::class.java))
-        }
-        binding.tvSignup.setOnClickListener {
-            startActivity(Intent(this@MainActivity,Register::class.java))
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (email.isEmpty()) {
+                binding.etEmail.error = "Kosong"
+                binding.etEmail.requestFocus()
+            } else if (password.isEmpty()){
+                binding.etPassword.error = "Kosong"
+                binding.etPassword.requestFocus()
+            } else {
+                //login
+                AndroidNetworking.post("http://192.168.1.8/api_perpus/login.php")
+                    .addBodyParameter("email", email)
+                    .addBodyParameter("password", password)
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsJSONObject(object : JSONObjectRequestListener {
+                        override fun onResponse(response: JSONObject) {
+                            if(response.getInt("success") == 1){
+                                Toast.makeText(this@MainActivity, response.getString("pesan"), Toast.LENGTH_SHORT).show()
+                                finish()
+                            } else {
+                                Toast.makeText(this@MainActivity, response.getString("pesan"), Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                        override fun onError(error : ANError) {
+                            Toast.makeText(this@MainActivity,error.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    })
+            }
         }
     }
 }
